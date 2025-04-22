18ba8775af34b9a856fa610eb9ba417deb0d4751
Thunder30519 <151902242+Thunder30519@users.noreply.github.com>
2025-04-23 03:15:52 +1000

    Update Data_processing.py

--- Code/Data_processing.py
+++ Code/Data_processing.py
@@ -1,3 +1,34 @@
 import numpy as np
 import pandas as pd
 import gc
+
+guardian = pd.read_csv('C:/Users/Thunder/Desktop/essay/guardian_articles.csv', encoding='ISO-8859-1')
+bbc = pd.read_csv('C:/Users/Thunder/Desktop/essay/bbc.csv', encoding='ISO-8859-1')
+
+### to find the content about russia and ukraine ###
+def find_ur(element, sentence):
+    result = False
+    sentence = sentence.upper()
+    for i in element:
+        i = i.upper()
+        if i in sentence:
+            result = True
+        else:
+            continue
+    return result
+
+### clean the data ###
+guardian_new = guardian.dropna()
+bbc_new = bbc.dropna()
+
+### find the russia and ukraine news after the war ###
+element = {'Russ','ukra'}
+number_g = np.array([])
+number_b = np.array([])
+for j in guardian_politi.index:
+    if find_ur(element,guardian_new.iloc[:,4][j]):
+        number_g = np.append(number_g,j)
+for i in guardian_politi.index:
+    if find_ur(element,bbc_new.iloc[:,4][i]):
+        number_b = np.append(number_b,i)
+
